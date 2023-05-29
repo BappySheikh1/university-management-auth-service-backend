a5f7291a4da317c8dd0a0429efdb767e7d8e9f41
Your Name Main <you@example.com>
2023-05-29 02:21:51 -0700

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app:Application = express()
+const app: Application = express()
 
 app.use(cors())
 
 // parser
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Working SuccessFull!')
 })
-
 
 export default app
